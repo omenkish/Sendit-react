ae3ceafc4a4207bd792f89eef7c1dddab305ba73
omenkish <omenkish@gmail.com>
2019-04-11 13:04:55 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// export const baseUrl = 'https://eneojo-sendit.herokuapp.com/api/v1'; 
-export const baseUrl = 'http://localhost:5000/api/v1'; 
+export const baseUrl = 'https://eneojo-sendit.herokuapp.com/api/v1'; 
